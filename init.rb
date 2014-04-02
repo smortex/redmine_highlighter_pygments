ad0cf2b3b06e63c5c59fdb6f973dec7b7759a93b
Romain Tartière <romain@blogreen.org>
2014-04-02 18:10:16 +0200

    Fix project url.

--- init.rb
+++ init.rb
@@ -7,7 +7,7 @@ Redmine::Plugin.register :redmine_highlighter_pygments do
   author 'Romain Tartière'
   description 'Rely on Pygments for syntax highlighting'
   version '1.0.0'
-  url 'https://github.com/smortex/redmine-highlighter-pygments'
+  url 'https://github.com/smortex/redmine_highlighter_pygments'
   author_url 'http://romain.blogreen.org'
 
   requires_redmine :version_or_higher => '2.0.0'
